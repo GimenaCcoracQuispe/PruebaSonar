2ff93bc2b23f600d8e4b9426d9a902e8d385835b
Gimena Ccoracc Quispe <111783689+GimenaCcoracQuispe@users.noreply.github.com>
2025-06-24 12:47:24 -0500

    Update IssueServiceTest.java

--- src/test/java/pe/edu/vallegrande/issue/service/IssueServiceTest.java
+++ src/test/java/pe/edu/vallegrande/issue/service/IssueServiceTest.java
@@ -13,7 +13,7 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
-public class IssueServiceTest {
+class IssueServiceTest {
      @Mock
     private IssueRepository issueRepository;
 
