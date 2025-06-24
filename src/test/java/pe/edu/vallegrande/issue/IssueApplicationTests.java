c076b25e4bae99db305a955aec09fb721b6e6e7c
Gimena Ccoracc Quispe <111783689+GimenaCcoracQuispe@users.noreply.github.com>
2025-06-24 12:40:07 -0500

    Update IssueApplicationTests.java

--- src/test/java/pe/edu/vallegrande/issue/IssueApplicationTests.java
+++ src/test/java/pe/edu/vallegrande/issue/IssueApplicationTests.java
@@ -1,6 +1,5 @@
 package pe.edu.vallegrande.issue;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
